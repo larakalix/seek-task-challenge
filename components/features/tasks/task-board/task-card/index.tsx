6a2580e79663567acc434c5be2a2807d28bb7ac3
Ivan Lara <ukikalix@gmail.com>
2025-03-18 01:33:19 -0400

    ft/added user into task card

--- components/features/tasks/task-board/task-card/index.tsx
+++ components/features/tasks/task-board/task-card/index.tsx
@@ -38,6 +38,12 @@ export const TaskCard = ({ task }: Props) => {
                 >
                     {task.description}
                 </Typography>
+                <hr className="my-4 opacity-20" />
+                <Typography variant="subtitle2" color="success">
+                    <span className="font-semibold">
+                        Created by {task.user_name}
+                    </span>
+                </Typography>
             </CardContent>
             <CardActions className="flex justify-between">
                 {task.status !== TaskStatus.Done && <DeleteTask task={task} />}
