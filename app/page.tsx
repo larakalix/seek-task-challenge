1f4def239f194c1c9137826040f0c1ae60d72a86
Ivan Lara <ukikalix@gmail.com>
2025-03-19 16:19:25 -0400

    rm/server fetch cache issue on task grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryInstance } from "@/utils/query-client";
-import { tasksQueryConfig } from "@/services/tasks/query-config";
 import { TaskView } from "@/components/features/tasks/view";
 
 export default async function Home() {
-    const client = getQueryInstance({
-        staleTimeMinutes: 10,
-    });
-    await client.prefetchQuery(tasksQueryConfig());
-
-    return (
-        <HydrationBoundary state={dehydrate(client)}>
-            <TaskView />
-        </HydrationBoundary>
-    );
+    return <TaskView />;
 }
